62b3f0e0e7608b8655c9335a6e0b48104fad65b3
Kirill Timofeev <kt97679@gmail.com>
2021-03-24 20:46:28 -0700

    added cmd-short-plist-to-bytes, fixed comment in the test.s

--- test.s
+++ test.s
@@ -1,6 +1,8 @@
-    ;; gcc -nostdlib test.s && objdump -d a.out
+# gcc -nostdlib test.s && objdump -d a.out
     .globl  _start
     .type   _start, @function
 _start:
     .byte 0x50
     .byte 0x58
+    .byte 0x8b
+    .byte 0xfe
